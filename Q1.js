a9d81a9ab6f0217132763013452bee9a2fdf75ee
zeeshanghani <mzeeshanghani786@gmail.com>
2023-08-23 23:00:31 -0700

    array created

---
+++ Q1.js
@@ -0,0 +1,9 @@
+"use strict";
+// Q1 - Create a function that takes an array, an index, and a value as parameters.
+//  Inside the function, use the splice method to insert the value at the specified index
+//  in the array. Return the modified array.
+var Rolls = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+function list(Rolls) {
+    console.log(list.length);
+}
+list(2);
---
+++ Q1.ts
@@ -0,0 +1,11 @@
+// Q1 - Create a function that takes an array, an index, and a value as parameters.
+//  Inside the function, use the splice method to insert the value at the specified index
+//  in the array. Return the modified array.
+
+ var Rolls:number[]=[10,20,30,40,50,60,70,80,90,100]
+
+function list(Rolls:number)
+{
+    console.log(list.length)
+}
+list(2)
