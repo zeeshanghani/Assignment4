c159e56cf7f7babb0c80b8c613889b82c2838c98
zeeshanghani <mzeeshanghani786@gmail.com>
2023-08-25 21:44:56 -0700

    arry function learn

---
+++ arry.ts
@@ -0,0 +1,11 @@
+//Create a function that takes an array, an index, and a value as parameters.
+// Inside the function, 
+//use the splice method to insert the value at the specified index in the array.
+// Return the modified array
+
+
+var arry:number[]=[10,20,30,40,50,60,70,80,90,100]
+console.log(arry.length)
+arry.splice(5,0,110,45,55,34)
+console.log(arry)
+console.log(arry.length)
