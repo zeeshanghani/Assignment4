5cf5afe74329f75d11ca98344c5be4d89a534f21
zeeshanghani <mzeeshanghani786@gmail.com>
2023-08-25 20:39:21 -0700

    tax

--- function.ts
+++ function.ts
@@ -1,16 +1,21 @@
-// sum of 2 number 5 & 7 using function
-function calc(num1:number,num2:number) {
-    var result:number=num1+num2
-    console.log("result of sum is =",result)
-    return result
-}
-//calc(5,7)
-// calculate the an other number 12 & 18
-// calc(12,18)
-//add 2 number 1st program add 6 & second program 10
-var addresult=calc(5,7)
-addresult=addresult+6
-console.log("Add 1st Result in 6 is =",addresult)
-var addresult=calc(12,18)
-addresult=addresult+10
-console.log("Add 2st Result in 10 is =",addresult)
+// // sum of 2 number 5 & 7 using function
+// function calc(num1:number,num2:number) {
+//     var result:number=num1+num2
+//     console.log("result of sum is =",result)
+//     return result
+// }
+// //calc(5,7)
+// // calculate the an other number 12 & 18
+// // calc(12,18)
+// //add 2 number 1st program add 6 & second program 10
+// var addresult=calc(5,7)
+// addresult=addresult+6
+// console.log("Add 1st Result in 6 is =",addresult)
+// var addresult=calc(12,18)
+// addresult=addresult+10
+// console.log("Add 2st Result in 10 is =",addresult)
+function lossPrice(purchasePrice:number,salePrice:number){
+    var loss:number=purchasePrice-salePrice
+    console.log("losspercentage is =",(loss/purchasePrice)*100,"%")
+ }
+ lossPrice(4500,2500)
---
+++ tax.ts
@@ -0,0 +1,68 @@
+
+// it is greater than 100 then add 10% tax if greater than 200
+//then add 15% of tax so on up to if greater than 500
+//then add 25% of tax.
+//Where the tax amount will be calculated by the amount of bill.
+
+// function bill(unitConsumed:number, unitrate:number)
+// {
+//     var billAmount=(unitConsumed*unitrate)
+//     //console.log(billAmount)
+//     if (unitConsumed>=100 && unitConsumed<=200)
+//     {
+//         var tax1:number=.1*billAmount
+//         var totalBill:number=billAmount+tax1
+//         console.log("The Total bill with 10%Tax is =", totalBill)
+//     } 
+//     else if (unitConsumed>200 && unitConsumed<=300) {
+//         var tax2:number=.2*billAmount
+//         var totalBill2:number=billAmount+tax2
+//         console.log("The Total bill with 20% Tax is =", totalBill2)
+//     }
+
+//     else if(unitConsumed>300 )
+//     {
+//         var tax3:number=.35*billAmount
+//         var totalBill3:number=billAmount+tax3
+//         console.log("The Total bill with 35% Tax is =", totalBill3)
+
+//     }
+//     else
+//     {
+//         console.log("The Total bill with outTax is =", billAmount)
+//     }
+// }
+// bill(90,15)
+// bill(101,15)
+// bill(301,15)
+ function bill(unitConsumed:number, taxRate:number)
+{
+    //var unitConsumed:number=20
+    var unitRate:number=20
+    var billAmount=(unitConsumed*unitRate)
+     if (unitConsumed>=100 && unitConsumed<=200)
+     {
+         
+         var totalBill:number=billAmount+(billAmount*taxRate/100)
+         console.log("The Total bill with Tax is =", totalBill)
+     } 
+     else if (unitConsumed>200 && unitConsumed<=300) {
+         
+         var totalBill2:number=billAmount+(billAmount*taxRate/100)
+         console.log("The Total bill with 20% Tax is =", totalBill2)
+     }
+
+     else if(unitConsumed>300 )
+     {
+        
+         var totalBill3:number=billAmount+(billAmount*taxRate/100)
+         console.log("The Total bill with 35% Tax is =", totalBill3)
+
+     }
+     else
+     {
+        console.log("The Total bill with outTax is =", billAmount)
+     }
+ }
+ bill(10,0)
+ bill(150,15)
